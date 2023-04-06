b10d8d692c62ccbbcc57f9dbb088a95b9b13a97e
Oleksandr Lazebnyi <oleksandr_lazebnyi@epam.com>
2023-04-06 12:34:43 +0300

    [EDP-DDM-25182] Fixed approve install plan
    Change-Id: I46883822655df561d71e75fd2681bada17de8a12

--- scripts/approveInstallPlan.sh
+++ scripts/approveInstallPlan.sh
@@ -5,17 +5,21 @@ export GROUP_SYNC_OPERATOR_VERSION=$2
 export GROUP_SYNC_OPERATOR_NAME="group-sync-operator.v${GROUP_SYNC_OPERATOR_VERSION}"
 export NAMESPACE=$3
 
-
 if [ ${GLOBAL_EDP_PROJECT} == "control-plane" ]; then
-	sleep 10
-	export INSTALL_PLAN=$(oc get installplan -n ${NAMESPACE} -o=jsonpath="{.items[?(@.spec.clusterServiceVersionNames[0]=='${GROUP_SYNC_OPERATOR_NAME}')].metadata.name}")
-	if [[ ${INSTALL_PLAN} == '' ]]; then
-	   echo "Nothing to approve"
-	else
-	   if ! $(oc get installplan -n ${NAMESPACE} ${INSTALL_PLAN} -o=jsonpath='{.spec.approved}'); then
-		 oc patch installplan ${INSTALL_PLAN} -n ${NAMESPACE} --type merge --patch '{"spec":{"approved":true}}'
-	   fi
-	fi
+  sleep 10
+  export INSTALL_PLAN=$(oc get installplan -n ${NAMESPACE} -o=jsonpath="{.items[?(@.spec.clusterServiceVersionNames[0]=='${GROUP_SYNC_OPERATOR_NAME}')].metadata.name}")
+  if [[ ${INSTALL_PLAN} == '' ]]; then
+    echo "Nothing to approve"
+  else
+    for IP in ${INSTALL_PLAN}
+    do
+      if ! $(oc get installplan -n ${NAMESPACE} ${IP} -o=jsonpath='{.spec.approved}'); then
+        echo "Approved ${IP}"
+        oc patch installplan ${IP} -n ${NAMESPACE} --type merge --patch '{"spec":{"approved":true}}'
+      fi
+    done
+    sleep 10
+  fi
 else
-	echo "Approve install plan skipped"
+  echo "Approve install plan skipped"
 fi
